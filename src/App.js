a94589edfbc04a2409d05ed79f0fdc68087d8a91
rasheedawilson12 <rasheedawilson12@gmail.com>
2022-12-19 14:55:09 -0600

    search engine created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Cedra Wilson</small>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
